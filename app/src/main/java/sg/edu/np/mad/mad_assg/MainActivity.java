5d150eb8aad88aafe3263a1059132b96172fa92a
Rachelle Ang Rui Xuan <116044822+rei-shell@users.noreply.github.com>
2023-05-24 12:51:56 +0800

    did the splash screen and the logo and a bit of the start page

---
+++ app/src/main/java/sg/edu/np/mad/mad_assg/MainActivity.java
@@ -0,0 +1,15 @@
+package sg.edu.np.mad.mad_assg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+
+    }
+}
---
+++ app/src/main/java/sg/edu/np/mad/mad_assg/SplashScreen.java
@@ -0,0 +1,27 @@
+package sg.edu.np.mad.mad_assg;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class SplashScreen extends MainActivity{
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+
+}
