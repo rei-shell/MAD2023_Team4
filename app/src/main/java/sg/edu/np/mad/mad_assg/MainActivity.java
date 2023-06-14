69189bbb156a76bdc6d4e7c36e0fe84b01133817
EudoraYeow <S10242444@connect.np.edu.sg>
2023-06-14 08:44:06 +0800

    update on view history

--- app/src/main/java/sg/edu/np/mad/mad_assg/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/mad_assg/MainActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<String> myList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.startpage);
 
+        myList.add("");
     }
 }
