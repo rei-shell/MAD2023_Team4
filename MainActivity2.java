d44eb135f743c8a5ce1344f4fc6c7b7b82421d31
xinyang22 <117617152+xinyang22@users.noreply.github.com>
2023-07-21 12:29:27 +0800

    Added a poll

--- MainActivity2.java
+++ MainActivity2.java
@@ -7,10 +7,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import sg.edu.np.mad.mad_assg.R;
@@ -19,6 +22,9 @@ import sg.edu.np.mad.mad_assg.RecipeView;
 public class FeedBack_Result extends AppCompatActivity {
     TextView tvFeedback;
     RatingBar rbStars;
+
+    // Poll Overlay Dialog
+    private AlertDialog pollOverlayDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -95,6 +101,15 @@ public class FeedBack_Result extends AppCompatActivity {
                 shareRecipe();
             }
         });
+
+        // Find the floating poll button
+        Button btnPollFloating = findViewById(R.id.btnPollFloating);
+        btnPollFloating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPollOverlay();
+            }
+        });
     }
 
     @Override
@@ -106,6 +121,8 @@ public class FeedBack_Result extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
 
     private void shareRecipe() {
 
@@ -124,7 +141,53 @@ public class FeedBack_Result extends AppCompatActivity {
 
     }
 
+
     private String getRecipeId() {
-        return null;
+        return getPackageName();
+    }
+
+    // Method to show the poll overlay
+    private void showPollOverlay() {
+        // Inflate the poll overlay layout
+        View pollOverlayView = getLayoutInflater().inflate(R.layout.layout_poll_overlay, null);
+
+        // Find poll elements in the overlay layout
+        TextView tvPollQuestionOverlay = pollOverlayView.findViewById(R.id.tvPollQuestionOverlay);
+        RadioGroup radioGroupPollOverlay = pollOverlayView.findViewById(R.id.radioGroupPollOverlay);
+        Button btnSubmitPollOverlay = pollOverlayView.findViewById(R.id.btnSubmitPollOverlay);
+        Button btnClosePollOverlay = pollOverlayView.findViewById(R.id.btnClosePollOverlay);
+
+        // Handle poll submit button click
+        btnSubmitPollOverlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectedPollId = radioGroupPollOverlay.getCheckedRadioButtonId();
+                if (selectedPollId != -1) {
+                    RadioButton selectedPollOption = pollOverlayView.findViewById(selectedPollId);
+                    String pollResponse = selectedPollOption.getText().toString();
+                    // Save or process the poll response as needed
+                    // For this example, we'll just display a toast
+                    Toast.makeText(FeedBack_Result.this, "Thank you for your poll response: " + pollResponse, Toast.LENGTH_SHORT).show();
+                }
+                // Dismiss the poll overlay after the user submits the poll
+                pollOverlayDialog.dismiss();
+            }
+        });
+
+        // Handle poll close button click
+        btnClosePollOverlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Dismiss the poll overlay if the user closes it without submitting
+                pollOverlayDialog.dismiss();
+            }
+        });
+
+        // Create and show the poll overlay dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(pollOverlayView);
+        builder.setCancelable(false); // Prevent the user from closing the dialog by clicking outside
+        pollOverlayDialog = builder.create();
+        pollOverlayDialog.show();
     }
 }
